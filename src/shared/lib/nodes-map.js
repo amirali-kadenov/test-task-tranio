58132fa66c4a8b3778db55768048f97b42127ee9
Amir Kadenov <amirkadenov@gmail.com>
2025-06-23 18:42:00 +0500

    add: nodes-map

---
+++ src/shared/lib/nodes-map.js
@@ -0,0 +1,26 @@
+export class NodesMap {
+  #nodes = new Map()
+
+  constructor() {}
+
+  get(id) {
+    // Invalidate cached node if it's no longer in the document
+    if (this.#nodes.has(id)) {
+      const cachedNode = this.#nodes.get(id)
+      if (document.body.contains(cachedNode)) {
+        return cachedNode
+      } else {
+        this.#nodes.delete(id) // Remove stale node
+      }
+    }
+
+    const node = document.getElementById(id)
+    if (!node) {
+      console.warn(`Node with ID '${id}' not found.`)
+      return null
+    }
+
+    this.#nodes.set(id, node)
+    return node
+  }
+}
